fb1054d3cd74c145b7eb7f94588d377ee22aef8f
GugaMelo <gugamelocarvalho@gmail.com>
2015-03-20 11:59:47 -0300

    Create Ex3_medias2.py

---
+++ Ex3_medias2.py
@@ -0,0 +1,17 @@
+n=0
+i=0
+l=[]
+while(n>= 0):
+        a= float(raw_input('digite uma nota:  '))
+        
+        if(a>=0):
+                l.append(a)
+                i+=1
+        else:
+                n=-1
+soma=0
+media=0.0
+for j in range(i):
+    soma += l[j]
+    media = soma/i
+print ('sua media eh {0}'.format(media))
